28f35248ce3eace2c24311a8a01424fa950b061c
solomonkrow <metro317@live.com>
2023-11-20 14:42:37 -0600

    fixed icon and database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -22,7 +22,7 @@ export const putDb = async (content) => {
 
   const store = tx.objectStore('jate');
 
-  const request = store.add({ id: 1, value: content })
+  const request = store.put({ id: 1, value: content })
 
   const result = await request;
   console.log('Data saved to the database', result);
@@ -38,7 +38,7 @@ export const getDb = async () => {
 
   const store = tx.objectStore('jate');
 
-  const request = store.get();
+  const request = store.get(1);
 
   const result = await request;
   console.log('result.value', result?.value);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -41,7 +41,7 @@ module.exports = () => {
           {
             src: path.resolve("./src/images/logo.png"),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join("assets", "logo"),
+            destination: path.join("assets", "icons"),
           },
         ],
       }),
