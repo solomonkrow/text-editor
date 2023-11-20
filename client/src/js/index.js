181e6a7d12ce8211af3013cd58c393aa27309886
solomonkrow <metro317@live.com>
2023-11-20 10:03:27 -0600

    more code

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -26,8 +26,16 @@ if (typeof editor === 'undefined') {
 // Check if service workers are supported
 if ('serviceWorker' in navigator) {
   // register workbox service worker
-  const workboxSW = new Workbox('/src-sw.js');
+  const workboxSW = new Workbox('/service-worker.js');
   workboxSW.register();
 } else {
   console.error('Service workers are not supported in this browser.');
 }
+
+/* if(module.hot) {
+  module.hot.accept((err) => {
+    if(err) {
+      console.error('Cannot apply HMR update.', err);
+    }
+  });
+} */
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,29 +1,77 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const devMode = process.env.NODE_ENV !== "production";
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      
-    ],
-
+      new HtmlWebpackPlugin({
+        title: "J.A.T.E",
+        template: "./index.html",
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "service-worker.js",
+      }),
+      new WebpackPwaManifest({
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "Text editor that runs in the browser",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        start_url: "./",
+        publicPath: "./",
+        fingerprints: false,
+        icons: [
+          {
+            src: path.resolve("./src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "logo"),
+          },
+        ],
+      }),
+    ].concat(devMode ? [] : [new MiniCssExtractPlugin()]),
+    /* devServer: {
+      hot: only,
+    }, */
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: [
+            devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+            "css-loader",
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
       ],
     },
   };
